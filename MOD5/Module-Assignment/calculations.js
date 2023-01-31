d2efb6a6ae10d4a6bfb73dbe70ee10d41026b8b7
Jeremy <122074101+jeremycorpuz@users.noreply.github.com>
2023-01-31 03:55:24 -0800

    Add files via upload

---
+++ MOD5/Module-Assignment/calculations.js
@@ -0,0 +1,38 @@
+let table = prompt("Enter a number from 0-10 (must be a whole number)"); // Unit of table
+var operator = 'multiplication'; // Type of calculation
+var i = 1; // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>'; // Message
+
+// Invalid responses
+while (table > 10 || table > 0 == false) {
+  table = prompt("Number must be between 0-10. Try again.")
+}
+
+while (table % 1 != 0) {
+  table = prompt("Enter a valid whole number from 0-10")
+}
+
+// Calculations
+while (i < 11) {
+  msg += i + ' * ' + table + ' = ' + (i * table) + '<br />';
+  i++
+}
+
+// UNUSED CODE
+//if (operator === 'addition') {
+  // Do addition
+//while (i < 11) {
+//    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+//    i++;
+//  }
+//} else {
+  // Do multiplication
+//  while (i < 11) {
+//    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+//    i++;
+//  }
+//}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
